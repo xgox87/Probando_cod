be0037050d9687dee179491434e8fe4a49a31db5
marta <marta@LAPTOP-MIR4HO11>
2019-12-13 10:05:11 -0300

    primera prueba de conexion a git

---
+++ Probando_console_app/Probando_console_app/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Probando_console_app
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("hello world");
+        }
+    }
+}
